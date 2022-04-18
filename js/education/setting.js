e443caded9edc878afa8356562fca9bfe1dded36
frank <frankzk.zhu@gmail.com>
2022-04-18 17:15:24 -0400

    commit

--- js/education/setting.js
+++ js/education/setting.js
@@ -9,26 +9,26 @@ $(document).ready(function() {
 	var commonstyle = "<li><a style=\"color:white\" href=\"";
 	var linka1=commonstyle+"../education/signal-sector.html\">"
 	+"Sector Movement Combination</a>"
-	var titlea = "<li>Sector Movement Combination";
+	var titlea = "<li  style=\"color:black\">Sector Movement Combination";
 	var linka = titlea+ul+linka1+cul;
 	
 	var linkb1=commonstyle+"../education/signal-er.html\">"
 	+"Following Positive ER</a>";
 	var linkb2 = commonstyle+"../education/signal-positivenews.html\">"
 	+"Following Positive News</a>";
-	var titleb ="<li>Trading Signal Combination";
+	var titleb ="<li style=\"color:black\">Trading Signal Combination";
 	var linkb = titleb+ul+linkb2+cul;
 	
 	var linkc1=commonstyle+"../education/bbandbottom.html\">"
 	+"Bollinger Bands trading strategy combination</a>";
 	var linkc2=commonstyle+"../education/bbandnoise.html\">Bollinger Bands trading strategy: noise</a>";
-	var titlec="<li>Bollinger Bands Combination";
+	var titlec="<li style=\"color:black\">Bollinger Bands Combination";
 	var linkc = titlec+ul+linkc2+cul;
 	
 	var linkd1=commonstyle+"../education/other-doublelows.html\">Double lows</a>";
  	var linkd2 = commonstyle+"../education/other-oversold.html\">Oversold</a>"	
  	var linkd3=commonstyle+"../education/other-overbought.html\">Overbought</a>";	
-	var titled="<li>Other Trading Signals Combination";
+	var titled="<li style=\"color:black\">Other Trading Signals Combination";
 	var linkd = titled+ul+linkd1+linkd2+cul;
 	var text = headtext + ul+linka+linkb+linkc+linkd+cul;     
 	$("#commonlink").html(text);
